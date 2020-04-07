71bb3e190b112924b998904858eebed655f7ab4c
TienTruong9x <59884685+TienTruong9x@users.noreply.github.com>
2020-04-07 22:15:09 +0700

    View user

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const port =3000;
 app.set('views', './views')
 app.set('view engine', 'pug');
 
+var users=[
+    {id:0,name:'Truong'},
+    {id:1,name:'Nhung'}
+]
+
 app.get('/',(req,res)=>{
     res.render('index',{title:'Truong DZ'});
 });
+
+app.get('/user',(req,res)=>{
+    res.render('user/index',{users:users});
+});
+
+app.get('/user/search',(req,res)=>{
+    let q=req.query.q;
+    let userMatch=users.filter((user)=>{
+        return user.name.toLowerCase().indexOf(q.toLowerCase())!== -1;
+    });
+    res.render('user/index',{users:userMatch,query:q});
+});
+
+
 
 app.listen(port,()=>{
     console.log('server is runing');
